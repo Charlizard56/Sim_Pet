d78ae7fc59a0433dbecdfbc9539109711cca0fa6
DM <85085855+Charlizard56@users.noreply.github.com>
2024-02-15 18:38:00 -0500

    Sprites 0.5
    -Fixed Sprite not starting on the correct frame.
    -Cleaned Code
    -Moved Text Draw to Draw.py
    -Changed Caption

--- Draw.py
+++ Draw.py
@@ -2,18 +2,21 @@ import pygame
 
 #Sprites
 pet_img = pygame.image.load("d_sheet.png")
-#pet_img = pygame.transform.flip(pet_img,1,0)
 pet_spr = pet_img.get_width()/4-2
 
 
 grass_img = pygame.image.load("grass.png")
 grass_img = pygame.transform.scale(grass_img,(120,120))
 
-def draw(_screen,_pet_x,_spr_size,_sprite_frame):
+def draw(_screen,_pet_x,_spr_size,_sprite_frame,_basic_font,_pet):
     #Set
-    i = _screen.get_width()/100
+    i = _screen.get_width()/100 #12.8
     ground_width = 0
     a = 0
+
+    #Text
+    my_font = _basic_font.render(f"HP: {_pet.hp}\nHunger: {_pet.hunger}\nInjured: {_pet.injured}", 0, "white")
+    _screen.blit(my_font, (6, 0))
 
     # Grass
     try:
